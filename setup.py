4bf0cadc62da5cc45b37877a155f828df6626532
Kenneth Hoste <kenneth.hoste@ugent.be>
2012-11-07 12:02:02 +0100

    add copyright header to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,36 @@
+##
+# Copyright 2012 Ghent University
+# Copyright 2012 Kenneth Hoste
+#
+# This file is part of EasyBuild,
+# originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
+# with support of Ghent University (http://ugent.be/hpc),
+# the Flemish Supercomputer Centre (VSC) (https://vscentrum.be/nl/en),
+# the Hercules foundation (http://www.herculesstichting.be/in_English)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# http://github.com/hpcugent/easybuild
+#
+# EasyBuild is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# EasyBuild is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EasyBuild.  If not, see <http://www.gnu.org/licenses/>.
+##
+
+"""
+This script can be used to install easybuild, e.g. using:
+  easy_install --user .
+or
+  python setup.py --prefix=$HOME/easybuild
+"""
+
 import os
 from distutils import log
 
