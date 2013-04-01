70cc7fbb61864b6079a12c47506f3ce44c5a7bff
Kenneth Hoste <kenneth.hoste@ugent.be>
2013-04-01 14:14:35 +0200

    bump version to v1.3.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from distutils import log
 
 # note: release candidates should be versioned as a pre-release, e.g. "1.1rc1"
 # 1.1-rc1 would indicate a post-release, i.e., and update of 1.1, so beware
-VERSION = "1.2.0"
+VERSION = "1.3.0"
 
 # Utility function to read README file
 def read(fname):
@@ -76,8 +76,8 @@ install software in a structured and robust way. """,
                   ],
     platforms = "Linux",
     install_requires = [
-                        "easybuild-easyconfigs == 1.2.0.0",
-                        "easybuild-easyblocks == 1.2.0",
-                        "easybuild-framework == 1.2.0",  # order matters here, framework should be after easyblocks!
+                        "easybuild-easyconfigs == 1.3.0.0",
+                        "easybuild-easyblocks == 1.3.0",
+                        "easybuild-framework == 1.3.0",  # order matters here, framework should be after easyblocks!
                        ]
 )
