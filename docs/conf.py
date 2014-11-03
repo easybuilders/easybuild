aa1fce6c841e5bd24db0dcd27360918a61a6cfe5
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-11-03 23:45:02 +0100

    fix html_title

--- docs/conf.py
+++ docs/conf.py
@@ -91,7 +91,7 @@ else:
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "<project> <version> , documentation version <release>"
+html_title = "|project| version |version| documentation (version |release|)"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
