fc93a64e3506f1bc587ef2f4e3577ad49acdc43b
Fotis Georgatos <fotis@cern.ch>
2014-11-03 10:29:04 +0100

    scrolls theme
    Signed-off-by: Fotis Georgatos <fotis@cern.ch>

--- conf.py
+++ conf.py
@@ -85,7 +85,7 @@ pygments_style = 'borland'
 # html_style = 'default.css'
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if on_rtd:
-    html_theme = 'traditional'
+    html_theme = 'scrolls'
 else:
     html_theme = 'default'
 
