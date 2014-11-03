ad523ee3a1e299c7dd51744abc3aa6af797166af
Kenneth Hoste <kenneth.hoste@ugent.be>
2014-11-03 15:39:04 +0100

    bump minor version

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ copyright = '2012-2014, Ghent University, CC-BY-SA'
 # The short X.Y version.
 version = '1.15.2' ## this is meant to reference the version of EasyBuild
 # The full version, including alpha/beta/rc tags.
-release = '0.6.1'  ## this is meant to reference the version of the documentation itself
+release = '0.6.2'  ## this is meant to reference the version of the documentation itself
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
