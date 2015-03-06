96ca318b5da25209f002a059be7b5a9eeead2ba4
Kenneth Hoste <kenneth.hoste@ugent.be>
2015-03-06 21:46:11 +0100

    bump version to v2.0.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ from distutils import log
 
 # note: release candidates should be versioned as a pre-release, e.g. "1.1rc1"
 # 1.1-rc1 would indicate a post-release, i.e., and update of 1.1, so beware
-VERSION = "1.16.2"
+VERSION = "2.0.0"
 
 # Utility function to read README file
 def read(fname):
@@ -76,8 +76,8 @@ on High Performance Computing (HPC) systems in an efficient way.""",
     ],
     platforms = "Linux",
     install_requires = [
-        "easybuild-easyconfigs == 1.16.2.0",
-        "easybuild-easyblocks == 1.16.2",
-        "easybuild-framework == 1.16.2",  # order matters here, framework should be after easyblocks!
+        "easybuild-easyconfigs == 2.0.0",
+        "easybuild-easyblocks == 2.0.0",
+        "easybuild-framework == 2.0.0",  # order matters here, framework should be after easyblocks!
     ]
 )
