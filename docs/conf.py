d1537df5f90cc8e8aef784da946cf30b104eb05c
Kenneth Hoste <kenneth.hoste@ugent.be>
2017-08-24 10:54:58 +0200

    bump to 3.4.0dev

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ copyright = '2012-2017, Ghent University, CC-BY-SA'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '3.3.1'  # this is meant to reference the version of EasyBuild
+version = '3.4.0dev'  # this is meant to reference the version of EasyBuild
 # The full version, including alpha/beta/rc tags.
 release = '20170824.01'  # this is meant to reference the version of the documentation itself
 
