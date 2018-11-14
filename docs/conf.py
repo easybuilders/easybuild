12d42717750db46bb4d29d5681b2de83e65300ef
Kenneth Hoste <kenneth.hoste@ugent.be>
2018-11-14 12:16:31 +0100

    bump docs version to 20181114.01

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ copyright = '2012-2018, Ghent University, CC-BY-SA'
 # The short X.Y version.
 version = '3.8.0dev'  # this is meant to reference the version of EasyBuild
 # The full version, including alpha/beta/rc tags.
-release = '20181107.01'  # this is meant to reference the version of the documentation itself
+release = '20181114.01'  # this is meant to reference the version of the documentation itself
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
