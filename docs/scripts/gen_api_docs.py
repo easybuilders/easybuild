a9c28adeadfb9b59291f4c3dba530e97e2428d97
Alan O'Cais <a.ocais@fz-juelich.de>
2019-12-10 12:27:17 +0100

    Make print statements work with Py3

--- docs/scripts/gen_api_docs.py
+++ docs/scripts/gen_api_docs.py
@@ -28,6 +28,8 @@ This script requires Sphinx to be installed (http://sphinx-doc.org/index.html).
 
 @author Caroline De Brouwer (Ghent University)
 """
+from __future__ import print_function
+
 import os
 import subprocess
 
@@ -49,5 +51,5 @@ if so.options.module is None:
 # -f forces to overwrite existing files
 # -e specifies that every module is written in a separate file
 cmd = "sphinx-apidoc -o %s %s -f -e" % (so.options.out_folder, so.options.module)
-print "$ %s" % cmd
+print("$ %s" % cmd)
 subprocess.call(cmd, shell=True)
