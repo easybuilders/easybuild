8a91090d3b51603397238dd4da06f735b93f401c
Kenneth Hoste <kenneth.hoste@ugent.be>
2025-03-18 21:23:23 +0100

    bump version to 5.0.0 (+ remove Python 2.7 + Python 3.5 from classifiers in setup.py)

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ from distutils.core import setup
 
 # note: release candidates should be versioned as a pre-release, e.g. "1.1rc1"
 # 1.1-rc1 would indicate a post-release, i.e., and update of 1.1, so beware
-VERSION = '4.9.4'
+VERSION = '5.0.0'
 
 
 # Utility function to read README file
@@ -69,8 +69,6 @@ on High Performance Computing (HPC) systems in an efficient way.""",
         "Intended Audience :: System Administrators",
         "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
